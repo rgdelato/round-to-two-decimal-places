cf23fb56478e6f0020d40ffb778b5e286f8b7389
Ryan De La Torre <rgdelato@gmail.com>
2016-03-01 20:03:47 -0800

    switching deepEqual()s to just normal equal()s

--- index.js
+++ index.js
@@ -10,43 +10,43 @@ var roundToTwoDecimalPlaces = function (num) {
 
 test('Testing roundToTwoDecimalPlaces()', function (assert) {
 
-	assert.deepEqual(
+	assert.equal(
 		roundToTwoDecimalPlaces(10),
 		"10",
 		'10 should round to "10"'
 	);
 
-	assert.deepEqual(
+	assert.equal(
 		roundToTwoDecimalPlaces(1.7777777),
 		"1.78",
 		'1.7777777 should round to "1.78"'
 	);
 
-	assert.deepEqual(
+	assert.equal(
 		roundToTwoDecimalPlaces(9.1),
 		"9.1",
 		'9.1 should round to "9.1"'
 	);
 
-	assert.deepEqual(
+	assert.equal(
 		roundToTwoDecimalPlaces(1.005),
 		"1.01",
 		'1.005 should round to "1.01"'
 	);
 
-	assert.deepEqual(
+	assert.equal(
 		roundToTwoDecimalPlaces(1.004),
 		"1",
 		'1.004 should round to "1"'
 	);
 
-	assert.deepEqual(
+	assert.equal(
 		roundToTwoDecimalPlaces(55.555),
 		"55.56",
 		'55.555 should round to "55.56"'
 	);
 
-	assert.deepEqual(
+	assert.equal(
 		roundToTwoDecimalPlaces(1.0049999999999999),
 		"1",
 		'1.0049999999999999 should round to "1"'
